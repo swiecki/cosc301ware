297f245f1b4e98cc33da92ba51c9a68402f6c923
Joel Sommers <jsommers@colgate.edu>
2012-08-29 11:14:54 -0400

    add overflow example from class

---
+++ examples/week01/overflow.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    unsigned int y;
+    unsigned char x = 130;
+    x *= 2;
+    printf("x is %d\n", x);
+    printf("y is %d\n", y);
+    return 0;
+}
