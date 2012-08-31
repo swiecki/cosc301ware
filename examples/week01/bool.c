5803111cde843ddaecc47c332ac96429fe905a25
Joel Sommers <jsommers@colgate.edu>
2012-08-31 10:30:37 -0400

    Add an example about the lack of booleans in C.

---
+++ examples/week01/bool.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int x = 0;
+    if (x = 10) 
+        printf("Java is for coffee\n");
+    else
+        printf("Not computers\n");
+    return 0;
+}
