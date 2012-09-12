3d5109835a0dcb7231fd217ec50510324c2cc868
dswiecki <dswiecki@deborah.(none)>
2012-09-12 15:37:45 -0400

    added lab 2 stuff

---
+++ labs/lab02/prob05.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node {
+    char name[128];
+    struct node *next; 
+};
+
+void insert(char *name,  ... ) {
+
+
+}
+
+void print_list(struct node *list) {
+    int i = 0;
+    printf("\n\nDumping list\n");
+    while (list != NULL) {
+        printf("%d: %s\n", i+1, list->name);
+        list = list->next;
+    }
+}
+
+void clear_list(struct node *list) {
+    while (list != NULL) {
+        struct node *tmp = list;
+        list = list->next;
+        free(tmp);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    char buffer[128];
+
+    struct node *head = NULL;
+
+    printf("Next string to add: ");
+    fflush(stdout);
+    while (fgets(buffer, 128, stdin) != NULL) {
+
+        insert(buffer, ... );
+
+        printf("Next string to add: ");
+        fflush(stdout);
+    }
+
+    print_list(head);
+    clear_list(head);
+
+    return 0;
+}
